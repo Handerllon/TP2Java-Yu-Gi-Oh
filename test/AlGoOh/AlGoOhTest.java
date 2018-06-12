ffe0a897da345ba406565ae391a39d98689a99e6
Ignacio Santiago Husain <santiago.husain@gmail.com>
2018-06-12 15:13:03 -0300

    Subo nombres de tests.

---
+++ test/AlGoOh/AlGoOhTest.java
@@ -0,0 +1,55 @@
+package AlGoOh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AlGoOhTest {
+
+    @Test
+    public void test01() {
+        assertTrue(0 == 0);
+    }/*Colocar una carta de monstruo en posición de ataque, el oponente coloca otra carta
+    de monstruo en posición de ataque (con mayor ataque). Atacar al primer monstruo y
+    verificar que este se destruyó, y sufro daño a los puntos de vida igual a la diferencia
+    de los puntos de ataque de los monstruos*/
+
+    @Test
+    public void test02() {
+        assertTrue(0 == 0);
+    }/*Colocar una carta de monstruo en posición de ataque, el oponente coloca otra carta
+    de monstruo en posición de ataque (con menor ataque), atacar al primer monstruo,
+    verificar que el monstruo atacante es destruido y el atacante recibe daño a los
+    puntos de vida igual a la diferencia de ataques.*/
+
+    @Test
+    public void test03() {
+        assertTrue(0 == 0);
+    }/*Colocar una carta de monstruo en posición de ataque, el oponente coloca otra carta
+    de monstruo en posición de ataque (con igual ataque), atacar al primer monstruo,
+    verificar que ambos monstruos son destruidos y nadie recibe daño a los puntos de
+    vida.*/
+
+    @Test
+    public void test04() {
+        assertTrue(0 == 0);
+    }/*Colocar una carta de monstruo en posición de defensa, el oponente coloca otra carta
+    de monstruo en posición de ataque (con mayor ataque que la defensa del primer
+            monstruo), atacar al primer monstruo y verificar que este se destruyó y no sufrió
+    ningún daño vital.*/
+
+    @Test
+    public void test05() {
+        assertTrue(0 == 0);
+    }/*Colocar una carta de monstruo en posición de defensa, el oponente coloca otra carta
+    de monstruo en posición de ataque (con menor ataque que la defensa del primer
+            monstruo), atacar al primer monstruo y verificar que este no se destruyó y no sufrió
+    ningún daño vital.*/
+
+    @Test
+    public void test06() {
+        assertTrue(0 == 0);
+    }/*Colocar monstruos en ambos lados del campo. Colocar Agujero negro boca arriba
+            (es decir, se activa el efecto). Verificar que se destruyeron todos los monstruos de
+    ambos lados del campo, y que nadie recibió daño alguno.*/
+}
--- test/clases/JugadorTest.java
+++ test/clases/JugadorTest.java
@@ -2,12 +2,46 @@ package clases;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class JugadorTest {
 
     @Test
-    public void test1() {
+    public void test01SeColocaUnaCartaMonstruoEnAtaqueYLaMismaQuedaEnAtaque() {
         assertTrue(0 == 0);
     }
+
+    @Test
+    public void test02() {
+        assertTrue(0 == 0);
+    }//Colocar un monstruo en posición de defensa
+
+    @Test
+    public void test03() {
+        assertTrue(0 == 0);
+    }//Colocar una carta mágica en el campo boca abajo (no activa ningún efecto)
+
+    @Test
+    public void test04() {
+        assertTrue(0 == 0);
+    }//Colocar una carta trampa en el campo boca abajo
+
+    @Test
+    public void test05() {
+        assertTrue(0 == 0);
+    }//Mandar una carta al cementerio (sinónimo de destruir) y verificar que estén ahí.
+
+    @Test
+    public void test06() {
+        assertTrue(0 == 0);
+    }/*Se coloca un monstruo en el campo, se quiere colocar un monstruo de 5 o 6
+    estrellas que requiere sacrificio. se verifica que se convocó al monstruo y se
+    destruyó el primero.*/
+
+    @Test
+    public void test07() {
+        assertTrue(0 == 0);
+    }/*Se colocan 2 monstruos en el campo, se quiere colocar un monstruo de 7 o más
+    estrellas que requiere 2 sacrificios. se verifica que se convocó al monstruo y se
+    destruyeron los demás.*/
 }
